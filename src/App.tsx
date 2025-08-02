270518a30fb59e61dff21bdc200c91003c3753ae
ryoyamasuda <ryouya_masuda@unirita.co.jp>
2025-08-03 01:13:35 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
-import { useState } from "react"
-import Title from "./components/Title"
-import Form from "./components/Form" 
-import Results from "./components/Results"
-import Loading from "./components/Loading" 
+import Title from './component/Title'; 
 
-type ResultsState = {  
-    country: string
-    cityName: string
-    temperature: string
-    conditionText: string 
-    icon: string
-}  
-
-const App = () => {
-    const [loading, setLoading] = useState<boolean>(false)
-    const [city, setCity] = useState<string>("")
-    const [results, setResults] = useState<ResultsState>({
-        country: "",
-        cityName: "",
-        temperature: "", 
-        conditionText: "",
-        icon: ""
-    })
-
-    const getWeather = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()  
-        setLoading(true) 
-        fetch(`https://api.weatherapi.com/v1/current.json?key=xxyyzz&q=${city}&aqi=no`) 
-            .then(res => res.json())
-            .then(data => {
-                setResults({
-                    country: data.location.country,
-                    cityName: data.location.name,
-                    temperature: data.current.temp_c,
-                    conditionText: data.current.condition.text,
-                    icon: data.current.condition.icon
-                })
-                setLoading(false) 
-            })
-    } 
-
+const APP = () => {
     return (
-        <div className="wrapper">  
-            <div className="container">
-                <Title/>
-                <Form setCity={setCity} getWeather={getWeather}/>
-                {loading ? <Loading/> : <Results results={results}/>}
-            </div>
+        <div>
+            <Title></Title>
         </div>
     )
 }
 
-export default App
+export default APP
