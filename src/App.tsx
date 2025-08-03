813f00d4987f965a37911beff85a31b069a29464
あなたの名前 <your_email@example.com>
2025-08-04 08:05:41 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import Title from './component/Title'; 
+import Form from './component/Forms';
+import Result from './component/Results';
+import { useState } from 'react';
 
 const APP = () => {
+    
+    const [city, setCity] = useState<string>("")
+
+    const getWeather = (e: any) => {
+        e.preventDefault()
+        fetch("http://localhost:3001/weather")
+            .then(res => res.json()).then(data => console.log(data))
+    }
+
     return (
         <div>
-            <Title></Title>
+            <Title/>
+            <Form setCity={setCity} getWeather={getWeather}/>
+            <Result/>
         </div>
     )
 }
